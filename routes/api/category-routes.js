7b33e7440cc5dccbc7bab8402694eeed8cf341ff
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-04-30 11:29:52 -0400

    updated category routes create a update category prompt

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,17 +30,28 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
 try{
-  const CategoryData = await Category.create(req.body);
-  res.status(200).json(CategoryData);
+  const createCategory = await Category.create(req.body);
+  res.status(200).json(createCategory);
 }catch (err){
   res.status(400).json(err);
 }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+try{
+  const updateCategory = await Category.update({
+    category_name: req.body.category_name
+  },
+  { where:{
+  id: req.params.id,
+ }   
 });
-router.delete('/:id', (req, res) => {
+res.status(200).json(updateCategory)
+} catch (err) {
+  res.status(500).json(err)
+}
+});
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
