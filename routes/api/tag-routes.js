f091fa515e9271fcc944ca5e42bde4e97a7b7757
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-04-30 12:54:58 -0400

    in tag routes added a find all tag data prompt

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,12 +3,18 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+try {
+  const TagData = await Tag.findAll({
+    include: [{model: Product}]
+  })
+  res.status(200).json(TagData)
+}catch (err){
+  res.status(500).json(err);
+}
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
